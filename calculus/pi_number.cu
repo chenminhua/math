72d9075665aa85fa61287b0db9a9d6d9de47be39
chenminhua <chenmh@blacklake.cn>
2019-08-23 14:14:12 +0800

    rename

--- calculus/pi_number.cu
+++ calculus/pi_number.cu
@@ -6,7 +6,7 @@
 #define THREAD_NUM 256 // 每个块中的线程数
 #define LOOP_N BLOCK_NUM * THREAD_NUM * 1000000
 
-__global__ void pi2(double* result) {
+__global__ void leib_pi(double* g_sum) {
     const int tid = threadIdx.x;
     const int bid = blockIdx.x;
     double tmp = 0;
@@ -18,31 +18,31 @@ __global__ void pi2(double* result) {
         tmp += flag * (1./(2 * i + 1));
         flag = -flag;
     }
-    result[bid*THREAD_NUM+tid] = tmp;
+    g_sum[bid*THREAD_NUM+tid] = tmp;
 }
 
 int main(){
-    double *sum, *result;
+    double *h_sum, *g_sum;
     double pi_v = 1;
 
     // allocate host memory
-    sum = (double*) malloc(sizeof(double) * BLOCK_NUM * THREAD_NUM);
+    h_sum = (double*) malloc(sizeof(double) * BLOCK_NUM * THREAD_NUM);
 
     // Allocate device memory
-    cudaMalloc((void **)&result, sizeof(double) * BLOCK_NUM * THREAD_NUM);
-    
+    cudaMalloc((void **)&g_sum, sizeof(double) * BLOCK_NUM * THREAD_NUM);
+
     // Execute kernels
-    pi2<<<BLOCK_NUM,THREAD_NUM>>>(result);
+    leib_pi<<<BLOCK_NUM,THREAD_NUM>>>(g_sum);
 
     // Transfer output from device memory to host
-    cudaMemcpy(sum, result, sizeof(double)*BLOCK_NUM*THREAD_NUM, cudaMemcpyDeviceToHost);
+    cudaMemcpy(h_sum, g_sum, sizeof(double)*BLOCK_NUM*THREAD_NUM, cudaMemcpyDeviceToHost);
 
     for (int i = 0; i < BLOCK_NUM * THREAD_NUM; i++) {
-        pi_v += sum[i];
+        pi_v += h_sum[i];
     }
 
     printf("calculate %.10f\n", pi_v*4);
 
-    cudaFree(result);
-    free(sum);
+    cudaFree(g_sum);
+    free(h_sum);
 }
